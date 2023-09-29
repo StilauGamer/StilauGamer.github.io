4b452646806cd29e83a78230cf700a1775d120fa
StilauGamer <stianlauvdal@hotmail.com>
2023-09-29 09:20:37 +0200

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div className='flex justify-center items-center h-full w-full'>
+        <h1 className="text-3xl font-bold">I am pro</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
